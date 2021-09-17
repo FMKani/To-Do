cc2b9807f7afae1ea2e78104a138a6ce12d48277
josé catanão <josecatanaoneto@gmail.com>
2021-09-17 02:13:00 -0300

    Primeiro Commit
    Adicionando:
    - Contagem de tarefas;
    - Adicionar uma nova tarefa;

---
+++ Atividade2-ReactNative-expo-main/src/components/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import logoImg from '../assets/images/logo/logo.png';
+let tasksCounterText;
+
+interface HeaderProps {
+  tasksCounter: number;
+}
+
+export function Header({ tasksCounter }: HeaderProps) {
+
+  if(tasksCounter==1){
+      tasksCounterText ='tarefa';
+  }else{
+      tasksCounterText ='tarefas';
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} />
+      
+      <View style={styles.tasks}>
+        <Text style={styles.tasksCounter}>Você tem </Text>
+        { <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: getStatusBarHeight(true) + 16,
+    paddingHorizontal: 24,
+    paddingBottom: 60,
+    backgroundColor: '#8257E5',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  tasks: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  tasksCounter: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter_400Regular',
+  },
+  tasksCounterBold: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter_700Bold',
+  }
+});
---
+++ Atividade2-ReactNative-expo-main/src/pages/Home.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Header } from '../components/Header';
+import {TasksList } from '../components/TasksList';
+//import {Task} from '../components/TaskItem';
+
+import { TodoInput } from '../components/TodoInput';
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    
+    const newtask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }  
+
+
+      const taskfound = tasks.findIndex(task => task.title === newTaskTitle)
+
+      if (taskfound !== -1) {
+
+        Alert.alert(
+          'task invalida !',
+          'você não pode inserir uma task com o mesmo nome.'
+        )
+
+      } else {
+
+        setTasks([...tasks, newtask]); 
+
+
+      }
+
+  }
+
+  function handleToggleTaskDone(id: number) {
+    //TODO - toggle task done if exists
+       
+  }
+
+  function handleRemoveTask(id: number) {
+    //TODO - remove task from state
+  }
+
+  
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList 
+        tasks={tasks} 
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
